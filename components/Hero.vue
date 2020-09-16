5e1c0f04bf0b421a9349fd30c40b6d987d7a8010
nori122 <norito.sasaki.jp@gmail.com>
2020-09-16 19:21:30 +0900

    theme customed

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="py-3">
-    <h1 color="primary--text">
-      個人クリエイターのためのコンテンツ翻訳サービス
-    </h1>
-    <v-card-subtitle-1>
-      高精度機械翻訳とネイティブスピーカーにより、あなたの作品を世界に届けます。
-    </v-card-subtitle-1>
-    <img class="heroimg" src="@/assets/img/heroimg.png" />
+  <div>
+    <v-container fluid class="py-3 primary--text">
+      <v-row>
+        <v-col>
+          <h1>個人クリエイターのためのコンテンツ翻訳サービス</h1>
+          <!-- <v-btn color="primary secondary--text">test</v-btn> -->
+          <v-card-subtitle-1>
+            高精度機械翻訳とネイティブスピーカーにより、あなたの作品を世界に届けます。
+          </v-card-subtitle-1>
+          <img class="heroimg" src="@/assets/img/heroimg.png" />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -18,8 +23,5 @@ export default {
 <style scoped>
 .heroimg {
   width: 100vw;
-}
-div {
-  /* background: linear-gradient(white, lightskyblue); */
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -73,8 +73,18 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      // disable: false,
+      light: true,
       themes: {
+        light: {
+          primary: '#1a3674',
+          accent: colors.grey.darken3,
+          secondary: '#e1c68d',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
@@ -84,15 +94,6 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
         },
-        // wings: {
-        //   primary: colors.red,
-        //   accent: colors.grey.darken3,
-        //   secondary: colors.amber.darken3,
-        //   info: colors.teal.lighten1,
-        //   warning: colors.amber.base,
-        //   error: colors.deepOrange.accent4,
-        //   success: colors.green.accent3,
-        // },
       },
     },
   },
