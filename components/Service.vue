f42bb87ecfe692a5290392a520afe074dc5158af
nori122 <norito.sasaki.jp@gmail.com>
2020-09-16 18:22:15 +0900

    0916_1820_サービスページ作成

--- components/Service.vue
+++ components/Service.vue
@@ -24,8 +24,30 @@
               <v-img align="center" class="pa-3">
                 <img class="service-image" src="../assets/img/mt.svg"
               /></v-img>
+              <v-card-text>DeepLを使用して機械翻訳を行います。</v-card-text>
+            </v-card>
+            <v-row align="center" justify="center" class="py-8"
+              ><v-icon size="30">mdi-arrow-down-thick</v-icon></v-row
+            >
+            <v-card>
+              <v-card-title>3. ネイティブチェック</v-card-title>
+              <v-img align="center" class="pa-3">
+                <img class="service-image" src="../assets/img/check.svg"
+              /></v-img>
               <v-card-text
-                >コンタクトフォームからお申し込みください。</v-card-text
+                >英語を母語とするネイティブスピーカーにより、より自然な英語へとブラッシュアップします。</v-card-text
+              >
+            </v-card>
+            <v-row align="center" justify="center" class="py-8"
+              ><v-icon size="30">mdi-arrow-down-thick</v-icon></v-row
+            >
+            <v-card>
+              <v-card-title>4. 翻訳完了</v-card-title>
+              <v-img align="center" class="pa-3">
+                <img class="service-image" src="../assets/img/done.svg"
+              /></v-img>
+              <v-card-text
+                >翻訳が完了したブログ記事をお渡しします。Mediumや、Indiehackersに投稿したり、ご自身のブログに掲載し、世界中の話者へ届けましょう。</v-card-text
               >
             </v-card>
           </v-col>
