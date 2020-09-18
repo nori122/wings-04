831d888e651c1bc6e5e23d7c58c7141e64da1c15
nori122 <norito.sasaki.jp@gmail.com>
2020-09-18 13:58:13 +0900

    0918 _1356_コンタクトフォームの追加

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,9 +1,22 @@
 <template>
-  <v-container fluid class="primary secondary--text">
+  <v-container fluid class="primary--text">
     <v-row align="center" justify="center" class="my-6"
       ><v-col>
-        <v-row>
-          <v-col> <h1>お問い合わせ</h1></v-col></v-row
+        <!-- <v-row>
+          <v-col> <h1>お問い合わせ</h1></v-col>
+        </v-row> -->
+        <v-row
+          ><v-col cols="12" md="6">
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSf8HgI2uX6dsILYlFDl_HPckqAocUwmpB_G6UqwRDb6iJFLtw/viewform?embedded=true"
+              width="100%"
+              height="1000"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+              >読み込んでいます…</iframe
+            >
+          </v-col></v-row
         >
       </v-col></v-row
     >
