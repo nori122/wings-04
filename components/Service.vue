53e7d9f63ace8ab41b566dfbcb061da72568a674
nori122 <norito.sasaki.jp@gmail.com>
2020-09-20 18:23:30 +0900

    0920_1820_serviceのデザイン完了

--- components/Service.vue
+++ components/Service.vue
@@ -1,65 +1,74 @@
 <template>
-  <v-container fluid class="primary--text">
+  <v-container fluid class="">
     <v-row align="center" justify="center" class="ma-2 my-6"
       ><v-col>
-        <v-row>
-          <v-col> <h1>サービスの流れ</h1></v-col></v-row
-        >
-        <v-row>
-          <v-col>
-            <v-card>
-              <v-card-title>1. 翻訳のお申し込み</v-card-title>
-              <v-img align="center" class="pa-3">
-                <img class="service-image" src="../assets/img/contact.svg"
-              /></v-img>
-              <v-card-text
-                >コンタクトフォームからお申し込みください。</v-card-text
+        <h1 align="center" class="my-5 font-weight-medium secondary--text">
+          SERVICE
+        </h1>
+        <!-- <v-card class="mx-auto my-2" max-width="344" outlined>
+          <v-img class="service-image pa-3" src="img/mt.svg" />
+        </v-card> -->
+      </v-col>
+      <v-col v-for="(step, i) in steps" :key="i" cols="12">
+        <v-card primary>
+          <v-row>
+            <v-col cols="12">
+              <v-card-title class="headline"
+                ><v-icon class="mr-5"
+                  >mdi-numeric-{{ step.number }}-circle</v-icon
+                >{{ step.title }}</v-card-title
               >
-            </v-card>
-            <v-row align="center" justify="center" class="py-8"
-              ><v-icon size="30">mdi-arrow-down-thick</v-icon></v-row
-            >
-            <v-card align="center">
-              <v-card-title>2. 機械翻訳を実施</v-card-title>
-              <v-img class="service-image pa-3" src="img/mt.svg" />
-              <v-card-text>DeepLを使用して機械翻訳を行います。</v-card-text>
-            </v-card>
-            <v-row align="center" justify="center" class="py-8"
-              ><v-icon size="30">mdi-arrow-down-thick</v-icon></v-row
-            >
-            <v-card>
-              <v-card-title>3. ネイティブチェック</v-card-title>
-              <v-img align="center" class="pa-3">
-                <img class="service-image" src="../assets/img/check.svg"
-              /></v-img>
-              <v-card-text
-                >英語を母語とするネイティブスピーカーにより、より自然な英語へと校正します。</v-card-text
-              >
-            </v-card>
-            <v-row align="center" justify="center" class="py-8"
-              ><v-icon size="30">mdi-arrow-down-thick</v-icon></v-row
-            >
-            <v-card>
-              <v-card-title>4. 翻訳完了</v-card-title>
-              <v-img align="center" class="pa-3">
-                <img class="service-image" src="../assets/img/done.svg"
-              /></v-img>
-              <v-card-text
-                >翻訳されたコンテンツをお渡しします。Mediumや、Indiehackersに投稿したり、ご自身のブログに掲載し、世界中の話者へ届けましょう。</v-card-text
-              >
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-col></v-row
-    >
+              <v-card-text v-text="step.detail"></v-card-text>
+            </v-col>
+            <v-col class="px-12 pb-12">
+              <v-img :src="step.src"></v-img>
+            </v-col>
+          </v-row>
+        </v-card> </v-col
+    ></v-row>
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    steps: [
+      {
+        number: 1,
+        title: '翻訳のお申し込み',
+        detail: 'コンタクトフォームからお申し込みください。',
+        src: 'img/contact.svg',
+      },
+      {
+        number: 2,
+        title: '機械翻訳を実施',
+        detail: 'DeepLを使用して機械翻訳を行います。',
+        src: 'img/mt.svg',
+      },
+      {
+        number: 3,
+        title: 'ネイティブチェック',
+        detail:
+          '英語を母語とするネイティブスピーカーにより、より自然な英語へと校正します。',
+        src: 'img/check.svg',
+      },
+      {
+        number: 4,
+        title: '翻訳完了',
+        detail:
+          '翻訳されたコンテンツをお渡しします。Mediumや、Indiehackersに投稿したり、ご自身のブログに掲載し、世界中の話者へ届けましょう。',
+        src: 'img/done.svg',
+      },
+    ],
+  }),
+}
 </script>
 <style scoped>
-.service-image {
-  width: 60%;
+/* .service-image {
+  width: 100%;
+} */
+h1 {
+  font-family: 'Futura', serif;
+  font-size: 50px;
 }
 </style>
